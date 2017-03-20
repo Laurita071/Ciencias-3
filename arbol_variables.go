d34e165b992a791d2be40528609addf761874887
Laura Cucanchon <laurita071@hotmail.es>
2017-03-19 22:20:19 -0500

    Update arbol_variables.go

--- arbol_variables.go
+++ arbol_variables.go
@@ -1,102 +1,184 @@
-//laura cucanchon
-//kevin parrado
+//Laura Marcela Cucanchón - 20131020037
+//Kevin parrado - 20131020033
 
 package main
 
 import (
-	"fmt"
+  "fmt"
+  "strconv"
+  "bufio"
+  "os"
   "strings"
-	"bufio"
-	"os"
 )
 
-type Arbol struct {
-	Izquierda  *Arbol
-	Valor string
-	Derecha *Arbol
+var v *StackV
+
+type Variable struct {
+  notac string
+  valor string
 }
 
-func New() *Arbol {
-	return &Arbol{}
+type Arbol struct{
+  Izquierda *Arbol
+  Valor string
+  Derecha *Arbol
+}
+
+type Stack struct {
+	nodes []*Arbol
+	count int
+}
+
+type StackV struct {
+	nodes []*Variable
+	count int
 }
 
 func NewStack() *Stack {
 	return &Stack{}
 }
 
-type Stack struct {
-	nodes []string
-	count int
+func NewStackV() *StackV {
+	return &StackV{}
 }
 
-func (s *Stack) Push(valor string) {
-	s.nodes = append(s.nodes[:s.count], valor)
+func (s *Stack) Push(n *Arbol) {
+	s.nodes = append(s.nodes[:s.count], n)
 	s.count++
 }
 
-func (s *Stack) Pop() string {
+func (s *Stack) Pop() *Arbol {
 	if s.count == 0 {
-		return ""
+		return nil
 	}
 	s.count--
 	return s.nodes[s.count]
 }
 
-func insertar(s *Stack, t *Arbol) *Arbol {
-	for i:=0; i < len(s.nodes);i++{
-		if t.Valor == "" {		
-			t=&Arbol{nil, s.Pop(), nil}
-			fmt.Println("raiz: ",t.Valor)
-		} else if t.Izquierda == nil{
-			t.Izquierda=&Arbol{nil, s.Pop(), nil}
-			fmt.Println("izq: ",t.Izquierda.Valor)
-		}else if t.Derecha == nil{
-			t.Derecha=&Arbol{nil, s.Pop(), nil}
-			fmt.Println("der: ",t.Derecha.Valor)
-		}
-	}
-	return t
+func (s *StackV) PushV(n *Variable) {
+	s.nodes = append(s.nodes[:s.count], n)
+	s.count++
 }
 
-func RecorrerInorden(t *Arbol) {
-	if t == nil {
-		return
+func (s *StackV) PopV() *Variable {
+	if s.count == 0 {
+		return nil
 	}
-	RecorrerInorden(t.Izquierda)
-	fmt.Print(t.Valor)
-  fmt.Print(" - ")
-	RecorrerInorden(t.Derecha)
+	s.count--
+	return s.nodes[s.count]
 }
 
-func RecorrerPreorden(t *Arbol) {
-	if t == nil {
-		return
-	}
-  fmt.Print(t.Valor)
-  fmt.Print(" - ")
-	RecorrerPreorden(t.Izquierda)
-	RecorrerPreorden(t.Derecha)
+func RecorrerInorden(t *Arbol, inorden string) string{
+  if t == nil{
+    return inorden
+  }else{
+    inorden=RecorrerInorden(t.Izquierda, inorden)
+    inorden=inorden+t.Valor
+    inorden=RecorrerInorden(t.Derecha, inorden)
+  }
+  return inorden
 }
 
-func InsertarExp(arr []string) *Stack{
-	st:=NewStack()
-	for _, r := range arr{
-		st.Push(r)
-	}
-	return st
+func CalcularArbol(t *Arbol) int{
+  if t.Izquierda!=nil && t.Derecha!=nil{
+    switch t.Valor{
+      case "+":
+          return CalcularArbol(t.Izquierda) + CalcularArbol(t.Derecha)
+      case "-":
+          return CalcularArbol(t.Izquierda) - CalcularArbol(t.Derecha)
+      case "*":
+          return CalcularArbol(t.Izquierda) * CalcularArbol(t.Derecha)
+      case "/":
+          return CalcularArbol(t.Izquierda) / CalcularArbol(t.Derecha)
+    }
+  }
+  i,e:=strconv.Atoi(t.Valor)
+  if(e != nil){
+    return valorVar(t.Valor)
+  }
+  return i
 }
 
-func main() {
-	scanner:= bufio.NewScanner(os.Stdin)
+func valorVar(vlr string) int{
+  for _, r:= range v.nodes {
+    if r.notac==vlr {
+      x,_:=strconv.Atoi(r.valor)
+      return x
+    }else{
+      return 0
+    }
+  }
+  return 0
+}
+
+func preevalComp(t *Arbol) string{
+  if t == nil{
+    return "No existe ningun arbol para evaluar\n"
+  } else if t.Izquierda == nil && t.Derecha == nil{
+    return "El nodo "+ t.Valor + " no tiene hijos\n"
+  } else if t.Izquierda !=nil && t.Derecha ==nil {
+    return "El arbol se encuentra incompleto, el nodo "+t.Valor+" no tiene hijo derecho\n"
+  } else if t.Izquierda ==nil && t.Derecha !=nil{
+    return "El arbol se encuentra incompleto, el nodo "+ t.Valor+" no tiene hijo izquierdo\n"
+  } else {
+    return preevalComp(t.Izquierda)+""+preevalComp(t.Derecha)
+  }
+  return " "
+}
+
+func operarPostfijo(op string) string{
+  var x string
+  c:=strings.Split(op, " ")
+  x, c = c[len(c)-2], c[:len(c)-2]
+  //v.PushV(&Variable{x,0})
+  fmt.Println("C: ",c)
+  if(len(c)<=2){
+    return "Error, no existen suficientes datos"
+  }
+  s := NewStack()
+  var contador int = 0
+  for i:=0; i< len(c); i++{
+    if(c[i]=="+" || c[i]=="-" || c[i]=="*" || c[i]=="/"){
+      contador++
+      if(s.count<2){
+        return "Error, no existe la cantidad necesaria de valores para calcular"
+      }else{
+        s.Push(&Arbol{nil,c[i],nil})
+        var arbol1 *Arbol = s.Pop()
+        arbol1.Derecha = s.Pop()
+        arbol1.Izquierda = s.Pop()
+        var resultado = CalcularArbol(arbol1)
+        s.Push(&Arbol{nil, strconv.Itoa(resultado),nil})
+      }
+    }else{
+      s.Push(&Arbol{nil,c[i],nil})
+    }
+  }
+  if(contador==0){
+    return "Error, no existe expresión en el árbol dado"
+  }
+  vFinal:=s.Pop().Valor
+  v.PushV(&Variable{x,vFinal})
+
+  return (x + " = " + vFinal)
+}
+
+func main(){
+  fmt.Println("Continuar? s/n")
+  scanner:= bufio.NewScanner(os.Stdin)
   scanner.Scan()
-  op:=scanner.Text()
-	ar:=strings.Split(op," ")
-	fmt.Println(ar)
-	t:=New()
-	fmt.Println(t.Valor)
-	s:=InsertarExp(ar)
-	fmt.Println(s.nodes)
-	insertar(s, t)
-
-
+  sgr:= scanner.Text()
+  v=NewStackV()
+  for sgr=="s" || sgr=="S" {
+    fmt.Println("Inserte la expresión en posOrden")
+    scanner.Scan()
+    op:=scanner.Text()
+    fmt.Println(operarPostfijo(op))
+    for _,r:= range v.nodes{
+      fmt.Println(r)
+    }
+    fmt.Println("Continuar? s/n")
+    scanner.Scan()
+    sgr=scanner .Text()
+  }
 }
